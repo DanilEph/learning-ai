35f20162322b074cb21e375384647323f0b14c84
Danil Efremov <danileph@bk.ru>
2022-03-28 02:45:52 +0300

    Task-3 has been fixed

--- lab-1/task-3.py
+++ lab-1/task-3.py
@@ -10,5 +10,5 @@ while(1):
     dividing_period = input('Какой временной интервал между делениями клеток (в часах)? \n> ')
     period = input('Сколько прошло вренени с момента, когда клетка была одна (в часах)? \n> ')
     print '------------'
-    print 'Таким образом, общее количество клеток за равняется ' + str(how_many_cells(cell_mubers_per_division, dividing_period, period))
+    print 'Таким образом, общее количество клеток равняется ' + str(how_many_cells(cell_mubers_per_division, dividing_period, period))
     print '\n'
