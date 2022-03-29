1675d00cfdc4ff4a66f7451ad6a8a5faece8416d
Danil Efremov <danileph@bk.ru>
2022-03-29 13:49:31 +0300

    Has been done task-2

---
+++ lab-2/task-2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import logging
+
+
+def inputTwoWords(isRepeat = 'repeat'):
+    word_1 = raw_input('Введите первое слово: ')
+    word_2 = raw_input('Введите второе слов: ')
+
+    if (isRepeat == 'no-repeat' and word_1 == word_2):
+        logging.error('Вы ввели два одиноковых слова! Слова не могут быть одинаковыми!')
+    else:
+        return word_1, word_2
+
+def match_words(word_1, word_2):
+    length = 0
+    match_count = 0
+
+    if (len(word_1) <= len(word_2)):
+        length = len(word_1)
+    else:
+        length = len(word_2)
+
+    for i in range(length):
+        if (word_1[i] == word_2[i]):
+            match_count += 1
+        else:
+            break
+
+    return match_count
+
+variant = input('Слова могут быть одинаковыми? \n(1) да \n(2) нет \n> ')
+number_of_letters_in_result = 0
+
+if (variant == 1):
+    number_of_letters_in_result = match_words(*inputTwoWords())
+elif (variant == 2):
+    number_of_letters_in_result = match_words(*inputTwoWords('no-repeat'))
+else:
+    print('Такого пункта нет!')
+
+if (variant == 1 or variant == 2):
+    print('Колличество одинаковых начальных букв двух введенных слов равно ' + str(number_of_letters_in_result))
